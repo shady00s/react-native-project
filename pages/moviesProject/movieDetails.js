761c86336d09cbcf726a977936f8326b3deac6ca
shady00s <ssk2310@hotmail.com>
2022-07-27 00:05:34 +0200

    add movies details page

--- pages/moviesProject/movieDetails.js
+++ pages/moviesProject/movieDetails.js
@@ -1,14 +1,14 @@
-import React ,{useState,useEffect}from "react";
+import React, { useState, useEffect } from "react";
 import { axiosInestance } from '../../axios/inestance'
+import { MovieCard } from "../../components/movieCard";
+import { View, Text, Image, StyleSheet, TouchableOpacity, ScrollView } from "react-native";
 
-import { View ,Text,Image,StyleSheet} from "react-native";
-
-export function MovieDetails({route,navigation}){
+export function MovieDetails({ route, navigation }) {
     const { itemId } = route.params;
     const [movieDetails, setMovieDetails] = useState({})
     useEffect(() => {
 
-        axiosInestance.get('/'+itemId).
+        axiosInestance.get('/' + itemId).
             then((res) => {
                 setMovieDetails(() => res.data)
 
@@ -16,18 +16,72 @@ export function MovieDetails({route,navigation}){
             .catch(e => alert(e))
 
     }, [])
-    return(
+    return (
+
         <View>
-            <Image style={style.image} source={{uri:`https://image.tmdb.org/t/p/w500${movieDetails.poster_path}`}}/>
-            <Text>{movieDetails.title}</Text>
+
+            <View style={style.header}>
+                <TouchableOpacity style={style.backButton} onPress={() => navigation.goBack()}>
+                    <Text >Back</Text>
+                </TouchableOpacity>
+                <Text style={style.titleText}>{movieDetails.title}</Text>
+            </View>
+            <ScrollView>
+                <Image style={style.image} source={{ uri: `https://image.tmdb.org/t/p/w500${movieDetails.poster_path}` }} />
+                <Text>{movieDetails.title}</Text>
+                <Text>{movieDetails.overview}</Text>
+            </ScrollView>
+            <View>
+                <SimilierMovies id={itemId} />
+            </View>
         </View>
+
+
+    )
+}
+
+
+function SimilierMovies(props) {
+    const [popularMoviesList, setPopularMoviesTest] = useState([])
+    useEffect(() => {
+
+        axiosInestance.get('/' + props.id + '/similar').
+            then((res) => {
+                setPopularMoviesTest(() => res.data.results)
+
+            })
+            .catch(e => alert(e))
+
+    }, [])
+    return (<View>
+        <Text>Similar Movies</Text>
+        <ScrollView horizontal={true}>
+
+            {popularMoviesList.map((element, i) => <MovieCard element={element} key={i} />)}
+        </ScrollView>
+    </View>
     )
 }
 
 const style = StyleSheet.create({
-    image:{
-        width:"100%",
-        height:"80%",
-        resizeMode:"cover"
+    header: {
+        height: 60,
+        flexDirection: "row",
+        alignItems: "center",
+
+    },
+    titleText: {
+        marginLeft: 40,
+        color: "black",
+        fontSize: 17
+    },
+    backButton: {
+        margin: 20
+    },
+
+    image: {
+        width: "100%",
+        height: 430,
+        resizeMode: "contain"
     }
 })
